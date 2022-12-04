2acf7b368bb99d3e5fe07ce5501b433ede2b99a9
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-12-04 19:19:00 +0600

    add git action

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from mainapp.models import Product
+
+class ModelTest(TestCase):
+    
+    def setUp(self):
+        self.product = Product.objects.create(
+            name="MacBook", 
+            price=10
+        )
+
+    def test_product_model(self):
+        product = self.product
+        self.assertTrue(isinstance(product, Product))
+        
